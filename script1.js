d3758959905976beccf0fa39e1225fdc7bcd093c
kokulvkt <kokulvkt222@gmail.com>
2024-04-20 06:52:53 +0000

    changes have been made

--- script1.js
+++ script1.js
@@ -3,9 +3,13 @@ function addNumbers(num1, num2) {
     return num1 + num2;
 }
 
+function subNumbers(num1, num2) {
+    return num1 + num2;
+}
 // Call the function with two numbers and store the result in a variable
 var result = addNumbers(5, 3);
+var result1=subNumbers(20,18);
 
 // Output the result to the console
 console.log("The sum of 5 and 3 is: " + result);
-
+console.log("the difference of 20 and 18 :" + result1);
